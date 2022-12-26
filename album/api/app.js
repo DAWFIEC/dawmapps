127fdc98c73ac1b3a86ff0b1e4d7924c4af68ad6
Allan <aavendan@gmail.com>
2022-12-26 13:58:13 -0500

    hito7-api

--- album/api/app.js
+++ album/api/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var fotosRouter = require('./routes/fotos');
 var etiquetasRouter = require('./routes/etiquetas');
+var fotosRESTRouter = require('./rest/fotos');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/fotos', fotosRouter);
 app.use('/etiquetas', etiquetasRouter);
+app.use('/rest/fotos', fotosRESTRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
